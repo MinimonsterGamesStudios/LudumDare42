a0d2232030ade4500023a696c336eba1c08ddcdb
Ethan Tal Abramchick <jaffe413@gmail.com>
2018-08-11 13:24:05 +0300

    Added Player Movement
    Added jump and movement
    add ground detection for jump

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _jumpForce = 50f;
+    [SerializeField]
+    private float _moveForce = 50f;
+    private Rigidbody _rigidbody;
+    private bool _isGrounded = false;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Jump();
+        Move();
+    }
+
+    private void Jump()
+    {
+        bool shouldJump = Mathf.Abs(Input.GetAxis("Jump")) > 0.1f;
+        if (_isGrounded && shouldJump)
+        {
+            _rigidbody.AddForce(Vector3.up * _jumpForce);
+        }
+    }
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        if (Mathf.Abs(horizontalInput) > 0 || Mathf.Abs(verticalInput) > 0)
+        {
+            _rigidbody.AddForce(new Vector3(horizontalInput, 0, verticalInput) * _moveForce, ForceMode.VelocityChange);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            _isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            _isGrounded = false;
+        }
+    }
+}
